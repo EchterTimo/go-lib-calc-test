2f3b9f7693209aea9ff45beb269e225cdbace558
EchterTimo <EchterTimo@users.noreply.github.com>
2026-06-15 19:17:29 +0200

    first commit

---
+++ calculator.go
@@ -0,0 +1,13 @@
+package calculator
+
+// Number is a custom constraint that accepts both integers and floats.
+type Number interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
+		~float32 | ~float64
+}
+
+// Add takes two numbers of the same type and returns their sum.
+func Add[T Number](a, b T) T {
+	return a + b
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/EchterTimo/go-lib-calc-test
+
+go 1.18
